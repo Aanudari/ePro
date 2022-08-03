2413277f87fae369a6ad8af0dfe24be3b1c566d9
narmandakh.g <narmandakh.g@ddishtv.mn>
2022-08-03 15:34:57 +0800

    home page

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ContextProvider } from './contexts/ContextProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <ContextProvider>
+        <App />
+    </ContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,177 @@ import Navigation from "../components/Navigation";
 
 function Home() {
   return (
-    <div>
+    <div className="w-full">
       <Navigation />
-      <div>
-        <p>home page</p>
+      <div className="px-2">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facilis
+          repudiandae fugiat minus non quae iste, ipsum dolores reiciendis quos
+          cupiditate iusto laborum temporibus quo perferendis. Dignissimos iste
+          impedit voluptatibus enim alias earum, ad illum vitae amet ea
+          distinctio exercitationem minima voluptate eius magni aperiam
+          eligendi, sapiente molestias voluptas, officiis esse obcaecati
+          doloremque. At molestiae quo iste odio vel magnam illo temporibus
+          quisquam animi explicabo ratione tempora nam velit accusantium error
+          maiores laborum laboriosam quasi, officia inventore ea officiis
+          dolorem libero. Necessitatibus quam nam, nesciunt iste cumque
+          architecto quos repellendus debitis, consequatur tempora deserunt
+          magnam aut numquam, sint tenetur expedita!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facilis
+          repudiandae fugiat minus non quae iste, ipsum dolores reiciendis quos
+          cupiditate iusto laborum temporibus quo perferendis. Dignissimos iste
+          impedit voluptatibus enim alias earum, ad illum vitae amet ea
+          distinctio exercitationem minima voluptate eius magni aperiam
+          eligendi, sapiente molestias voluptas, officiis esse obcaecati
+          doloremque. At molestiae quo iste odio vel magnam illo temporibus
+          quisquam animi explicabo ratione tempora nam velit accusantium error
+          maiores laborum laboriosam quasi, officia inventore ea officiis
+          dolorem libero. Necessitatibus quam nam, nesciunt iste cumque
+          architecto quos repellendus debitis, consequatur tempora deserunt
+          magnam aut numquam, sint tenetur expedita!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facilis
+          repudiandae fugiat minus non quae iste, ipsum dolores reiciendis quos
+          cupiditate iusto laborum temporibus quo perferendis. Dignissimos iste
+          impedit voluptatibus enim alias earum, ad illum vitae amet ea
+          distinctio exercitationem minima voluptate eius magni aperiam
+          eligendi, sapiente molestias voluptas, officiis esse obcaecati
+          doloremque. At molestiae quo iste odio vel magnam illo temporibus
+          quisquam animi explicabo ratione tempora nam velit accusantium error
+          maiores laborum laboriosam quasi, officia inventore ea officiis
+          dolorem libero. Necessitatibus quam nam, nesciunt iste cumque
+          architecto quos repellendus debitis, consequatur tempora deserunt
+          magnam aut numquam, sint tenetur expedita!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facilis
+          repudiandae fugiat minus non quae iste, ipsum dolores reiciendis quos
+          cupiditate iusto laborum temporibus quo perferendis. Dignissimos iste
+          impedit voluptatibus enim alias earum, ad illum vitae amet ea
+          distinctio exercitationem minima voluptate eius magni aperiam
+          eligendi, sapiente molestias voluptas, officiis esse obcaecati
+          doloremque. At molestiae quo iste odio vel magnam illo temporibus
+          quisquam animi explicabo ratione tempora nam velit accusantium error
+          maiores laborum laboriosam quasi, officia inventore ea officiis
+          dolorem libero. Necessitatibus quam nam, nesciunt iste cumque
+          architecto quos repellendus debitis, consequatur tempora deserunt
+          magnam aut numquam, sint tenetur expedita!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facilis
+          repudiandae fugiat minus non quae iste, ipsum dolores reiciendis quos
+          cupiditate iusto laborum temporibus quo perferendis. Dignissimos iste
+          impedit voluptatibus enim alias earum, ad illum vitae amet ea
+          distinctio exercitationem minima voluptate eius magni aperiam
+          eligendi, sapiente molestias voluptas, officiis esse obcaecati
+          doloremque. At molestiae quo iste odio vel magnam illo temporibus
+          quisquam animi explicabo ratione tempora nam velit accusantium error
+          maiores laborum laboriosam quasi, officia inventore ea officiis
+          dolorem libero. Necessitatibus quam nam, nesciunt iste cumque
+          architecto quos repellendus debitis, consequatur tempora deserunt
+          magnam aut numquam, sint tenetur expedita!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facilis
+          repudiandae fugiat minus non quae iste, ipsum dolores reiciendis quos
+          cupiditate iusto laborum temporibus quo perferendis. Dignissimos iste
+          impedit voluptatibus enim alias earum, ad illum vitae amet ea
+          distinctio exercitationem minima voluptate eius magni aperiam
+          eligendi, sapiente molestias voluptas, officiis esse obcaecati
+          doloremque. At molestiae quo iste odio vel magnam illo temporibus
+          quisquam animi explicabo ratione tempora nam velit accusantium error
+          maiores laborum laboriosam quasi, officia inventore ea officiis
+          dolorem libero. Necessitatibus quam nam, nesciunt iste cumque
+          architecto quos repellendus debitis, consequatur tempora deserunt
+          magnam aut numquam, sint tenetur expedita!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facilis
+          repudiandae fugiat minus non quae iste, ipsum dolores reiciendis quos
+          cupiditate iusto laborum temporibus quo perferendis. Dignissimos iste
+          impedit voluptatibus enim alias earum, ad illum vitae amet ea
+          distinctio exercitationem minima voluptate eius magni aperiam
+          eligendi, sapiente molestias voluptas, officiis esse obcaecati
+          doloremque. At molestiae quo iste odio vel magnam illo temporibus
+          quisquam animi explicabo ratione tempora nam velit accusantium error
+          maiores laborum laboriosam quasi, officia inventore ea officiis
+          dolorem libero. Necessitatibus quam nam, nesciunt iste cumque
+          architecto quos repellendus debitis, consequatur tempora deserunt
+          magnam aut numquam, sint tenetur expedita!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facilis
+          repudiandae fugiat minus non quae iste, ipsum dolores reiciendis quos
+          cupiditate iusto laborum temporibus quo perferendis. Dignissimos iste
+          impedit voluptatibus enim alias earum, ad illum vitae amet ea
+          distinctio exercitationem minima voluptate eius magni aperiam
+          eligendi, sapiente molestias voluptas, officiis esse obcaecati
+          doloremque. At molestiae quo iste odio vel magnam illo temporibus
+          quisquam animi explicabo ratione tempora nam velit accusantium error
+          maiores laborum laboriosam quasi, officia inventore ea officiis
+          dolorem libero. Necessitatibus quam nam, nesciunt iste cumque
+          architecto quos repellendus debitis, consequatur tempora deserunt
+          magnam aut numquam, sint tenetur expedita!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facilis
+          repudiandae fugiat minus non quae iste, ipsum dolores reiciendis quos
+          cupiditate iusto laborum temporibus quo perferendis. Dignissimos iste
+          impedit voluptatibus enim alias earum, ad illum vitae amet ea
+          distinctio exercitationem minima voluptate eius magni aperiam
+          eligendi, sapiente molestias voluptas, officiis esse obcaecati
+          doloremque. At molestiae quo iste odio vel magnam illo temporibus
+          quisquam animi explicabo ratione tempora nam velit accusantium error
+          maiores laborum laboriosam quasi, officia inventore ea officiis
+          dolorem libero. Necessitatibus quam nam, nesciunt iste cumque
+          architecto quos repellendus debitis, consequatur tempora deserunt
+          magnam aut numquam, sint tenetur expedita!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facilis
+          repudiandae fugiat minus non quae iste, ipsum dolores reiciendis quos
+          cupiditate iusto laborum temporibus quo perferendis. Dignissimos iste
+          impedit voluptatibus enim alias earum, ad illum vitae amet ea
+          distinctio exercitationem minima voluptate eius magni aperiam
+          eligendi, sapiente molestias voluptas, officiis esse obcaecati
+          doloremque. At molestiae quo iste odio vel magnam illo temporibus
+          quisquam animi explicabo ratione tempora nam velit accusantium error
+          maiores laborum laboriosam quasi, officia inventore ea officiis
+          dolorem libero. Necessitatibus quam nam, nesciunt iste cumque
+          architecto quos repellendus debitis, consequatur tempora deserunt
+          magnam aut numquam, sint tenetur expedita!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facilis
+          repudiandae fugiat minus non quae iste, ipsum dolores reiciendis quos
+          cupiditate iusto laborum temporibus quo perferendis. Dignissimos iste
+          impedit voluptatibus enim alias earum, ad illum vitae amet ea
+          distinctio exercitationem minima voluptate eius magni aperiam
+          eligendi, sapiente molestias voluptas, officiis esse obcaecati
+          doloremque. At molestiae quo iste odio vel magnam illo temporibus
+          quisquam animi explicabo ratione tempora nam velit accusantium error
+          maiores laborum laboriosam quasi, officia inventore ea officiis
+          dolorem libero. Necessitatibus quam nam, nesciunt iste cumque
+          architecto quos repellendus debitis, consequatur tempora deserunt
+          magnam aut numquam, sint tenetur expedita!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facilis
+          repudiandae fugiat minus non quae iste, ipsum dolores reiciendis quos
+          cupiditate iusto laborum temporibus quo perferendis. Dignissimos iste
+          impedit voluptatibus enim alias earum, ad illum vitae amet ea
+          distinctio exercitationem minima voluptate eius magni aperiam
+          eligendi, sapiente molestias voluptas, officiis esse obcaecati
+          doloremque. At molestiae quo iste odio vel magnam illo temporibus
+          quisquam animi explicabo ratione tempora nam velit accusantium error
+          maiores laborum laboriosam quasi, officia inventore ea officiis
+          dolorem libero. Necessitatibus quam nam, nesciunt iste cumque
+          architecto quos repellendus debitis, consequatur tempora deserunt
+          magnam aut numquam, sint tenetur expedita!
+        </p>
       </div>
     </div>
   );

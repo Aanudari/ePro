405b2376ac9532a3541154e4b99c04f84f62cb8d
odmunkh <odmunkh.o@ddishtv.mn>
2022-12-07 18:48:34 +0800

    up

--- src/pages/error-thanks/CreateErrorThanks.jsx
+++ src/pages/error-thanks/CreateErrorThanks.jsx
@@ -19,7 +19,15 @@ function CreateErrorThanks() {
   const [startDate, setStartDate] = useState(new Date());
   const [department, setDepartment] = useState();
   const [org, setOrg] = useState();
-  const [selectedOption, setSelectedOption] = useState(null);
+  const [workers, setWorkers] = useState([]);
+  const [selectedOptiondepartment, setSelectedOptiondepartment] =
+    useState(null);
+
+  const [selectedOptionorg, setSelectedOptionorg] = useState(null);
+  const [selectedOptionWorkers, setSelectedOptionWorkers] = useState(null);
+  const [departmentID, setDepartmentID] = useState();
+  const [orgID, setOrgID] = useState();
+  const [workersID, setWorkersID] = useState();
   useEffect(() => {
     axios({
       method: "get",
@@ -36,22 +44,56 @@ function CreateErrorThanks() {
       })
       .catch((err) => console.log(err));
   }, []);
-  if (selectedOption) {
+  if (selectedOptiondepartment) {
     axios({
       method: "get",
       headers: {
         Authorization: `${TOKEN}`,
       },
-      url: `http://192.168.10.248:9000/v1/User/org/${selectedOption.id}`,
+      url: `http://192.168.10.248:9000/v1/User/org/${selectedOptiondepartment.id}`,
     })
       .then((res) => {
-        setOrg(res.data.result);
         if (res.data.resultMessage === "Unauthorized") {
           logout();
+        } else {
+          setOrg(res.data.organizations);
+          setDepartmentID(selectedOptiondepartment.id);
         }
       })
       .catch((err) => console.log(err));
   }
+  if (selectedOptionorg) {
+    axios({
+      method: "get",
+      headers: {
+        Authorization: `${TOKEN}`,
+      },
+      url: `http://192.168.10.248:9000/v1/User/unit/devices?unitId=${selectedOptionorg.id}`,
+    })
+      .then((res) => {
+        if (res.data.resultMessage === "Unauthorized") {
+          logout();
+        } else {
+          setWorkers(res.data.unitDevices);
+          setOrgID(selectedOptionorg.id);
+        }
+      })
+      .catch((err) => console.log(err));
+  }
+  if (selectedOptionWorkers) {
+    setWorkersID(selectedOptionWorkers.deviceId);
+  }
+  const [inputValues, setInputValue] = useState({
+    department: departmentID,
+    unit: orgID,
+    deviceId: workersID,
+    complain: location.state.type.category,
+    complainType: "gomdli trl garaar",
+    description: "gmdl del txt",
+    rule: "juram",
+    too: "1-5",
+    createdBy: deviceId,
+  });
   return (
     <div className="w-full h-screen bg-gray-50">
       <Navigation />
@@ -66,8 +108,8 @@ function CreateErrorThanks() {
         <div className="p-4 container max-w-screen-lg mx-auto">
           <div className="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6">
             <div className="lg:col-span-2">
-              <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-4">
-                <div className="mb:col-span-1">
+              <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
+                <div className="md:col-span-1">
                   <label>Огноо</label>
                   <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
                     <DatePicker
@@ -80,14 +122,17 @@ function CreateErrorThanks() {
                     />
                   </div>
                 </div>
-                <div className="mb:col-span-1">
-                  <label for="state">Харьяалагдах хэлтэс</label>
+                <div className="md:col-span-2">
+                  <label>Харьяалагдах хэлтэс</label>
                   <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1 ">
                     <Select
                       className="outline-none  w-full"
                       options={department}
-                      defaultValue={selectedOption}
-                      onChange={setSelectedOption}
+                      defaultValue={selectedOptiondepartment}
+                      onChange={setSelectedOptiondepartment}
+                      onSelectChange={(item) =>
+                        console.log("use your custom handler here", item)
+                      }
                       noOptionsMessage={({ inputValue }) =>
                         !inputValue && "Сонголт хоосон байна"
                       }
@@ -96,13 +141,13 @@ function CreateErrorThanks() {
                     />
                   </div>
                 </div>
-                <div className="mb:col-span-2">
-                  <label for="state">Ажлын байр</label>
+                <div className="md:col-span-1">
+                  <label>Ажлын байр</label>
                   <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
                     <Select
-                      options={department}
-                      defaultValue={selectedOption}
-                      onChange={setSelectedOption}
+                      options={org}
+                      defaultValue={selectedOptionorg}
+                      onChange={setSelectedOptionorg}
                       className="outline-none  w-full"
                       noOptionsMessage={({ inputValue }) =>
                         !inputValue && "Сонголт хоосон байна"
@@ -111,6 +156,51 @@ function CreateErrorThanks() {
                       getOptionValue={(option) => option.id}
                     />
                   </div>
+                </div>
+                <div className="md:col-span-1">
+                  <label>Ажилтны нэр</label>
+                  <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
+                    <Select
+                      options={workers}
+                      defaultValue={selectedOptionWorkers}
+                      onChange={setSelectedOptionWorkers}
+                      className="outline-none  w-full"
+                      noOptionsMessage={({ inputValue }) =>
+                        !inputValue && "Сонголт хоосон байна"
+                      }
+                      getOptionLabel={(option) => option.firstName}
+                      getOptionValue={(option) => option.deviceId}
+                    />
+                  </div>
+                </div>
+
+                <div className="md:col-span-2">
+                  <label>Гомдлын төрөл</label>
+                  <input
+                    type="text"
+                    className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                  />
+                </div>
+                <div className="md:col-span-2">
+                  <label>Журам</label>
+                  <input
+                    type="text"
+                    className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                  />
+                </div>
+                <div className="md:col-span-1">
+                  <label>Алдаа</label>
+                  <input
+                    type="text"
+                    className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                  />
+                </div>
+                <div className="md:col-span-5">
+                  <label>Гомдлын дэлгэрэнгүй</label>
+                  <textarea
+                    type="text"
+                    className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                  />
                 </div>
 
                 <div className="col-span-5 text-right">

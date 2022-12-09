0bdeacd4a2e69dd50dcc9117461606409d7bb1e6
odmunkh <odmunkh.o@ddishtv.mn>
2022-12-09 16:56:02 +0800

    update

--- src/pages/error-thanks/ErrorThanks.jsx
+++ src/pages/error-thanks/ErrorThanks.jsx
@@ -3,10 +3,12 @@ import Navigation from "../../components/Navigation";
 import { useStateContext } from "../../contexts/ContextProvider";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { Modal, Button } from "react-bootstrap";
+import { Modal } from "react-bootstrap";
 import Select from "react-select";
+import ReactPaginate from "react-paginate";
+import "./pg.css";
 function ErrorThanks() {
-  const { TOKEN, deviceId } = useStateContext();
+  const { TOKEN } = useStateContext();
   const navigate = useNavigate();
   const logout = () => {
     localStorage.clear();
@@ -14,16 +16,20 @@ function ErrorThanks() {
     navigate("/");
     window.location.reload();
   };
-
+  let color = "blue";
   const [currentTab, setCurrentTab] = useState("1");
   const [complainInfo, setComplainInfo] = useState();
-  const [complain, setComplain] = useState();
+  const [complain, setComplain] = useState([]);
   const [selectedOption, setSelectedOption] = useState(null);
   const [showCreate, setShowCreate] = useState(null);
   const showModalCreate = () => setShowCreate(true);
   const hideModalCreate = () => setShowCreate(null);
   const [showDelete, setShowDelete] = useState(null);
-  const showModalDelete = () => setShowDelete(true);
+  const [id, setId] = useState();
+  const showModalDelete = (e) => {
+    setShowDelete(true);
+    setId(e.currentTarget.dataset.id);
+  };
   const hideModalDelete = () => setShowDelete(null);
   useEffect(() => {
     axios({
@@ -65,8 +71,32 @@ function ErrorThanks() {
       state: { type: selectedOption },
     });
   };
-  let color = "blue";
-  console.log(complain);
+  const handleDelete = () => {
+    axios({
+      method: "delete",
+      headers: {
+        Authorization: `${TOKEN}`,
+        accept: "text/plain",
+      },
+      url: `http://192.168.10.248:9000/v1/Complain/delete?id=${id}`,
+    })
+      .then((res) => {
+        if (res.data.isSuccess === true) {
+          navigate(0);
+        } else {
+          console.log(res.data.resultMessage);
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+  const [page, setPage] = useState(0);
+  const dataPerPage = 5;
+  const numberOfdataVistited = page * dataPerPage;
+  const totalPages = Math.ceil(complain.length / dataPerPage);
+
+  const changePage = ({ selected }) => {
+    setPage(selected);
+  };
 
   return (
     <div className="w-full h-screen bg-gray-50">
@@ -134,8 +164,11 @@ function ErrorThanks() {
               <div className="px-5 pb-5">
                 <div className="flex justify-between items-center">
                   <span className="text-sm font-bold">Бүртгэл устгах уу?</span>
-                  <button className="text-white bg-rose-700 hover:bg-rose-600 focus:ring-4 focus:outline-none focus:ring-rose-300 font-medium rounded-lg text-sm px-2 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    DELETE
+                  <button
+                    onClick={handleDelete}
+                    className="text-white bg-rose-700 hover:bg-rose-600 focus:ring-4 focus:outline-none focus:ring-rose-300 font-medium rounded-lg text-sm px-2 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    YES
                   </button>
                 </div>
               </div>
@@ -144,6 +177,7 @@ function ErrorThanks() {
         </Modal>
       </div>
       <Navigation />
+
       <div className="sm:px-6 w-full">
         <div className="px-4 md:px-10 py-4 md:py-7">
           <div className="flex items-center justify-between">
@@ -151,43 +185,9 @@ function ErrorThanks() {
               Алдаа талархал
             </p>
             <div className="my-2 flex sm:flex-row flex-col">
-              <div className="flex flex-row mb-1 sm:mb-0">
-                <div className="relative">
-                  <select className="appearance-none h-full rounded-l border block appearance-none w-full bg-white border-gray-400 text-black py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
-                    <option>5</option>
-                    <option>10</option>
-                    <option>20</option>
-                  </select>
-                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-black">
-                    <svg
-                      className="fill-current h-4 w-4"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                    >
-                      <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                    </svg>
-                  </div>
-                </div>
-                <div className="relative">
-                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-black">
-                    <svg
-                      className="fill-current h-4 w-4"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                    >
-                      <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                    </svg>
-                  </div>
-                </div>
-              </div>
               <div className="block relative">
                 <span className="h-full absolute inset-y-0 left-0 flex items-center pl-2">
-                  <svg
-                    viewBox="0 0 24 24"
-                    className="h-4 w-4 fill-current text-black"
-                  >
-                    <path d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z"></path>
-                  </svg>
+                  <i className="bi bi-search" />
                 </span>
                 <input
                   placeholder="Хайлт"
@@ -207,7 +207,7 @@ function ErrorThanks() {
               >
                 {complainInfo
                   ? complainInfo.map((tab, i) => (
-                      <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+                      <li className="-mb-px mr-2 last:mr-2 mt-2 flex-auto text-center">
                         <a
                           className={
                             "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
@@ -238,66 +238,86 @@ function ErrorThanks() {
               Бүртгэл нэмэх
             </button>
           </div>
-          <div className="mt-7 overflow-x-auto">
+          <div className="mt-3 overflow-x-auto">
             <table className="items-center w-full bg-transparent border-collapse ">
               <thead>
-                <tr class="text-sm font-normal md:font-bold font-semibold tracking-wide text-left  bg-gray-100 uppercase border-b border-gray-600">
-                  <th class="px-4 py-3">Огноо </th>
-                  <th class="px-4 py-3">Харьяалагдах хэлтэс </th>
-                  <th class="px-4 py-3">Ажлын байр </th>
-                  <th class="px-4 py-3">Ажилтны нэр </th>
-                  <th class="px-4 py-3">Гомдлын төрөл </th>
-                  <th class="px-4 py-3">Гомдлын дэлгэрэнгүй </th>
-                  <th class="px-4 py-3">Журам </th>
-                  <th class="px-4 py-3">Алдаа </th>
-                  <th class="px-4 py-3">Action </th>
+                <tr className="text-sm text-left  bg-gray-200 border-b">
+                  <th className="px-4 py-3 font-bold">Огноо </th>
+                  <th className="px-4 py-3 font-bold">Харьяалагдах хэлтэс </th>
+                  <th className="px-4 py-3 font-bold">Ажлын байр </th>
+                  <th className="px-4 py-3 font-bold">Ажилтны нэр </th>
+                  <th className="px-4 py-3 font-bold">Гомдлын төрөл </th>
+                  <th className="px-4 py-3 font-bold">Гомдлын дэлгэрэнгүй </th>
+                  <th className="px-4 py-3 font-bold">Журам </th>
+                  <th className="px-4 py-3 font-bold">Алдаа </th>
+                  <th className="px-4 py-3 font-bold">Action </th>
                 </tr>
               </thead>
-              <tbody class="bg-white text-sm">
+              <tbody className="bg-white text-sm">
                 {complain
-                  ? complain.map((tab, i) => (
-                      <tr
-                        key={i}
-                        className={
-                          currentTab === `${tab.complain}`
-                            ? "focus:outline-none h-16 border border-gray-100 rounded"
-                            : "hidden"
-                        }
-                      >
-                        <td class="px-1 py-1 border">{tab.createdAt}</td>
-                        <td class="px-1 py-1 border">{tab.departmentName}</td>
-                        <td class="px-1 py-1 border">{tab.unitName}</td>
-                        <td class="px-1 py-1 border">{tab.firstName}</td>
-                        <td class="px-1 py-1 border">{tab.complainType}</td>
-                        <td class="px-1 py-1 border">{tab.description}</td>
-                        <td class="px-1 py-1 border">{tab.rule}</td>
-                        <td class="px-1 py-1 border">{tab.too}</td>
-                        <td class="px-1 py-1 border">
-                          <a className="text-yellow-400 hover:text-black mx-2">
-                            <i className="bi bi-pencil-square"></i>
-                          </a>
-                          <a
-                            onClick={showModalDelete}
-                            className="text-rose-400 hover:text-black ml-2"
-                          >
-                            <i className="bi bi-trash-fill"></i>
-                          </a>
-                        </td>
-                      </tr>
-                    ))
+                  ? complain
+                      .map((tab, i) => (
+                        <tr
+                          key={i}
+                          className={
+                            currentTab === `${tab.complain}`
+                              ? "focus:outline-none h-16 border border-gray-100 rounded"
+                              : "hidden"
+                          }
+                        >
+                          <td className="px-1 py-1 border">{tab.createdAt}</td>
+                          <td className="px-1 py-1 border">
+                            {tab.departmentName}
+                          </td>
+                          <td className="px-1 py-1 border">{tab.unitName}</td>
+                          <td className="px-1 py-1 border">{tab.firstName}</td>
+                          <td className="px-1 py-1 border">
+                            {tab.complainType}
+                          </td>
+                          <td className="px-1 py-1 border">
+                            {tab.description}
+                          </td>
+                          <td className="px-1 py-1 border">{tab.rule}</td>
+                          <td className="px-1 py-1 border">{tab.too}</td>
+                          <td className="px-1 py-1 border">
+                            <a className="text-yellow-400 hover:text-black mx-2">
+                              <i className="bi bi-pencil-square"></i>
+                            </a>
+                            <a
+                              data-id={tab.id}
+                              onClick={showModalDelete}
+                              className="text-rose-400 hover:text-black ml-2"
+                            >
+                              <i className="bi bi-trash-fill"></i>
+                            </a>
+                          </td>
+                        </tr>
+                      ))
+                      .slice(
+                        numberOfdataVistited,
+                        numberOfdataVistited + dataPerPage
+                      )
                   : null}
               </tbody>
             </table>
 
             <div className="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between">
-              <div className="inline-flex mt-2 xs:mt-0">
-                <button className="text-sm bg-gray-200 hover:bg-gray-300 text-gray-800 font-semibold py-2 px-4 rounded-l">
-                  Өмнөх
-                </button>
-                <button className="text-sm bg-gray-200 hover:bg-gray-300 text-gray-800 font-semibold py-2 px-4 rounded-r">
-                  Дараах
-                </button>
-              </div>
+              <ReactPaginate
+                previousLabel={"Өмнө"}
+                nextLabel={"Дараах"}
+                pageCount={totalPages}
+                onPageChange={changePage}
+                containerClassName={"navigationButtons"}
+                previousLinkClassName={"previousButton"}
+                nextLinkClassName={"nextButton"}
+                disabledClassName={"navigationDisabled"}
+                activeClassName={"navigationActive"}
+                breakLabel={"..."}
+                breakClassName={"break-me"}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                subContainerClassName={"pages pagination"}
+              />
             </div>
           </div>
         </div>

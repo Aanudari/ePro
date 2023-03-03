0cebc341420444913805a0f12c3cfffb7618f491
odmunkh <odmunkh.o@ddishtv.mn>
2023-03-03 10:16:18 +0800

    train rate report 50%

--- src/pages/training_raiting/RatingReport.jsx
+++ src/pages/training_raiting/RatingReport.jsx
@@ -8,15 +8,22 @@ import Select from "react-select";
 import { notification } from "../../service/toast";
 import { ToastContainer } from "react-toastify";
 import moment from "moment";
-import DatePicker from "react-datepicker";
 import { logout } from "../../service/examService";
-import getWindowDimensions from "../../components/SizeDetector";
+import { Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 function RatingReport() {
   const location = useLocation();
   const { TOKEN } = useStateContext();
   const navigate = useNavigate();
   const selectedRate = location.state.data;
   const [questionResults, setQuestionResults] = useState([]);
+  const [watched, setWatched] = useState([]);
+  const format = "YYYYMMDDHHmmss";
+  const today = new Date();
   useEffect(() => {
     axios({
       method: "get",
@@ -38,6 +45,27 @@ function RatingReport() {
       })
       .catch((err) => console.log(err));
   }, []);
+  useEffect(() => {
+    axios({
+      method: "get",
+      headers: {
+        Authorization: `${TOKEN}`,
+      },
+      url: `${process.env.REACT_APP_URL}/v1/TrainingReport/training/watched?trainingId=${selectedRate.trainingId}`,
+    })
+      .then((res) => {
+        if (
+          res.data.resultMessage === "Unauthorized" ||
+          res.data.resultMessage === "Input string was not in a correct format."
+        ) {
+          logout();
+        }
+        if (res.data.isSuccess === true) {
+          setWatched(res.data.watchedList);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
   const separatedArr = questionResults.reduce((acc, obj) => {
     const { questionType } = obj;
     if (!acc[questionType]) {
@@ -46,13 +74,14 @@ function RatingReport() {
     acc[questionType].push(obj);
     return acc;
   }, {});
+
   const type1Arr = separatedArr["1"];
   const type2Arr = separatedArr["2"];
-  console.log(selectedRate);
+
   return (
     <div className="w-full min-h-[calc(100%-56px)] ">
       <Navigation />
-      <div className="sm:px-6 w-full">
+      <div className="sm:px-6">
         <button
           onClick={() => navigate("/training-rating")}
           className="bg-white border border-white p-2 rounded text-gray-700 flex items-center focus:outline-none focus:shadow-outline "
@@ -75,8 +104,129 @@ function RatingReport() {
               Үнэлгээний тайлан
             </p>
           </div>
+          <div className="gap-8 md:flex">
+            <div className="mb-8 text-center md:w-1/3 md:mb-0">
+              <div className="p-2 text-gray-600 bg-white rounded-lg shadow-lg">
+                <p className="font-body text-black">Үнэлгээ өгөх хүний тоо</p>
+                <a className="text-blue-500 font-body hover:text-gray-800">
+                  <i className="bi bi-people" /> {watched.length}
+                </a>
+              </div>
+            </div>
+            <div className="text-center md:w-1/2">
+              <div className="p-2 text-gray-600 bg-white rounded-lg shadow-lg">
+                <p className="font-body text-black">Үнэлгээ өгсөн хүний тоо</p>
+                <a className="text-blue-500 font-body hover:text-gray-800">
+                  <i className="bi bi-people" /> ????
+                </a>
+              </div>
+            </div>
+            <div className="text-center md:w-1/2">
+              <div className="p-2 text-gray-600 bg-white rounded-lg shadow-lg">
+                <p className="font-body text-black">Статус</p>
+                <a className="text-blue-500 font-body hover:text-gray-800">
+                  {moment(today).format(format) >=
+                  moment(selectedRate.expireDate).format(format) ? (
+                    <span className="flex items-center px-2 py-1 text-xs font-semibold text-red-500 bg-red-200 rounded-md">
+                      ИДЭВХГҮЙ
+                    </span>
+                  ) : (
+                    <span className="flex items-center px-2 py-1 text-xs font-semibold inline-flex items-center justify-center rounded-full bg-emerald-100 text-emerald-700">
+                      ИДЭВХТЭЙ
+                    </span>
+                  )}
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-        aaa
+        <div>
+          <div className="flex items-center bg-gray-300 px-5 py-2 mb-2 text-lg font-medium text-gray-600 border border-blue-500  jusitfy-between">
+            <div className="flex items-center w-full">Section 1</div>
+          </div>
+
+          <div className="mx-auto max-w-screen-xl px-2 py-12 sm:px-6 lg:px-8">
+            <Swiper
+              modules={[Pagination, Scrollbar, A11y]}
+              spaceBetween={50}
+              slidesPerView={3}
+              pagination={{ clickable: true }}
+              scrollbar={{ draggable: true }}
+            >
+              {type1Arr?.map((data, index) => (
+                <SwiperSlide>
+                  <div key={index} className="bg-gray-100 p-4">
+                    <div className="flex items-center gap-4">
+                      <div className="text-sm">
+                        <p className="font-medium">
+                          {" "}
+                          Q{index + 1}. {data.question}
+                        </p>
+                      </div>
+                    </div>
+
+                    <ul className="mt-2">
+                      {data.answerResults1.map((a, i) => (
+                        <li key={i} className="text-sm">
+                          <div className="flex items-start">
+                            <div className="flex-1 mb-1">
+                              <div className="font-medium dark:text-white">
+                                {a.answerName}
+                              </div>
+                            </div>
+                            <div className="flex justify-end text-right">
+                              {a.answeredNumber === "" ? 0 : a.answeredNumber}
+                            </div>
+                          </div>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </SwiperSlide>
+              ))}
+
+              <div className="swiper-pagination !relative !bottom-0 mt-12"></div>
+            </Swiper>
+          </div>
+        </div>
+        <div>
+          <div className="flex items-center bg-gray-300 px-5 py-2 mb-2 text-lg font-medium text-gray-600 border border-blue-500  jusitfy-between">
+            <div className="flex items-center w-full">Section 1</div>
+          </div>
+
+          <div className="mx-auto max-w-screen-xl px-2 py-12 sm:px-6 lg:px-8">
+            <Swiper
+              modules={[Pagination, Scrollbar, A11y]}
+              spaceBetween={50}
+              slidesPerView={3}
+              pagination={{ clickable: true }}
+              scrollbar={{ draggable: true }}
+            >
+              {type2Arr?.map((data, index) => (
+                <SwiperSlide>
+                  <div key={index} className="bg-gray-100 p-8">
+                    <div className="flex items-center gap-4">
+                      <div className="text-sm">
+                        <p className="font-medium">
+                          {" "}
+                          Q{index + 1}. {data.question}
+                        </p>
+                      </div>
+                    </div>
+                    {data.answerResults2.map((a, i) => (
+                      <p key={i} className="relative mt-4 text-gray-500">
+                        <span className="text-xl">&ldquo;</span>
+                        {a.answerName}
+                        <span className="text-xl">&rdquo;</span>
+                      </p>
+                    ))}
+                  </div>
+                </SwiperSlide>
+              ))}
+              <div className="swiper-pagination !relative !bottom-0 mt-12"></div>
+            </Swiper>
+          </div>
+        </div>
       </div>
 
       <ToastContainer />

a1dea127c1e3f6b28412cc9f753df0f1d74ad51f
munkhbayar.b <munkhbayar.b@ddishtv.mn>
2023-03-14 14:53:54 +0800

    update

--- src/pages/rating/TemplateRelated/TemplateCategoryCell.jsx
+++ src/pages/rating/TemplateRelated/TemplateCategoryCell.jsx
@@ -1,9 +1,13 @@
 import TemplateSubCategoryCell from "./TemplateSubCategoryCell";
 
 function TemplateCategoryCell({ item, index }) {
+  console.log(item);
   return (
-    <div className="w-full flex bg-red-100 mt-2 justify-center flex-col">
-      <div className=" h-full">{JSON.stringify(item)}</div>
+    <div className="w-full flex  mt-2 justify-center flex-col">
+      <div className="flex "> 
+      <div className="grow basis-4/5 p-1 h-full w-3/4 bg-red-100">{item.categoryName + " " }</div>
+      <div className="grow basis-1/5 p-1 h-full w-3/4 bg-red-100">{item.categoryPoint + " " }</div>
+      </div>
       <div className="w-full h-full bg-red-200">
         {item.subCategories.map((element, i) => (
           <TemplateSubCategoryCell
--- src/pages/rating/TemplateRelated/TemplateSubCategoryCell.jsx
+++ src/pages/rating/TemplateRelated/TemplateSubCategoryCell.jsx
@@ -1,5 +1,5 @@
 function TemplateSubCategoryCell({ element }) {
-  return <div>{JSON.stringify(element)}</div>;
+  return <div className="button-red-200 btn-13">{JSON.stringify(element)}</div>;
 }
 
 export default TemplateSubCategoryCell;

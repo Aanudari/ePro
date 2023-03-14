c4a4edd4d36f0409a498cac341858f0fa8641238
munkhbayar.b <munkhbayar.b@ddishtv.mn>
2023-03-14 17:39:44 +0800

    update

--- src/pages/rating/TemplateRelated/TemplateCategoryCell.jsx
+++ src/pages/rating/TemplateRelated/TemplateCategoryCell.jsx
@@ -1,21 +1,30 @@
 import TemplateSubCategoryCell from "./TemplateSubCategoryCell";
+import { useState}  from "react"
+
 
 function TemplateCategoryCell({ item, index }) {
+
+  const [showSub, setShowSub] = useState(false);
+
+  function handleClick(){
+    setShowSub(!showSub);
+  }
+  
   console.log(item);
   return (
-    <div className="w-full flex  mt-2 justify-center flex-col">
-      <div className="flex "> 
-      <div className="grow basis-4/5 p-1 h-full w-3/4 bg-red-100">{item.categoryName + " " }</div>
-      <div className="grow basis-1/5 p-1 h-full w-3/4 bg-red-100">{item.categoryPoint + " " }</div>
+    <div className="mt-1 justify-center flex-col shadow">
+      <div className="flex justify-center bg-gray-100 hover:bg-gray-200 shadow" onClick={handleClick}> 
+        <div  className="w-4/5 from-left3 border text-center font-bold text-lg  h-full p-1 flex flex-col justify-between">{item.categoryName}</div>
+        <div  className="w-1/5 from-left4 border text-center font-bold text-lg  h-full p-1 flex flex-col justify-between">{item.categoryPoint + " %" }</div>
       </div>
-      <div className="w-full h-full bg-red-200">
+      {showSub && <div className="w-full h-full ">
         {item?.subCategories?.map((element, i) => (
           <TemplateSubCategoryCell
             element={element}
             key={JSON.stringify(item + i)}
           />
         ))}
-      </div>
+      </div>}
     </div>
   );
 }
--- src/pages/rating/TemplateRelated/TemplateSubCategoryCell.jsx
+++ src/pages/rating/TemplateRelated/TemplateSubCategoryCell.jsx
@@ -1,5 +1,10 @@
 function TemplateSubCategoryCell({ element }) {
-  return <div className="button-red-200 btn-13">{JSON.stringify(element)}</div>;
+  return <div className="flex">
+            <div className="text-center text-base from-left3 bg-white w-4/5 h-full  flex flex-col justify-between  text-gray-500 px-3 py-1 shadow text-[15px]">{element.subcategoryName}</div> 
+            <div className="text-center text-base from-left4 bg-white w-1/5 h-full  flex flex-col justify-between  text-gray-500 px-3 py-1 shadow text-[15px]">{element.subcategoryPoint + "%"}</div> 
+         </div>
+  
+
 }
 
 export default TemplateSubCategoryCell;
